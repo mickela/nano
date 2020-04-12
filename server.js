b4b0db73b311965231fd16a3ca01b60862a96e87
Michael <osoba.michael@outlook.com>
2020-04-13 00:49:39 +0100

    ...minor change

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const db = require('./config/db');
+// const db = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 
 // db.authenticate().then(res => console.log('PostgreSQL connected successfully')).catch(err => console.log(err))
 
@@ -10,6 +14,14 @@ app.disable('x-powered-by');
 // routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.reslove(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server started on localhost:${PORT}`));
