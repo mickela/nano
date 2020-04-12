d593e29d27834bc73155b72690e63ab05ff712cd
Michael <osoba.michael@outlook.com>
2020-04-12 15:48:06 +0100

    Created delete UI

--- client/src/components/layouts/UrlUI.jsx
+++ client/src/components/layouts/UrlUI.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Moment from 'react-moment';
 
-export default function UrlUI({ shortUrl, longUrl, date, copy }){
+export default function UrlUI({ id, shortUrl, longUrl, date, copy, remove }){
+    const [deleted, setDeleted] = useState(false);
     return(
-        <div className="url-card alert alert-info">
+        <div className="url-card alert alert-info" style={{ display: deleted ? 'none' : null }}>
             <Line title="Short Url" detail={shortUrl} />
             <Line title="Long Url" detail={longUrl} />
             <Line title="Date" detail={<Moment date={date}/>} />
             <button className="btn btn-outline-primary" onClick={()=> copy(shortUrl)}>copy url</button>
+            &nbsp;
+            <button className="btn btn-outline-danger" onClick={()=> { remove(id); setDeleted(true); }}>delete url</button>
         </div>
     )
 }
--- client/src/components/logic/Url.jsx
+++ client/src/components/logic/Url.jsx
@@ -14,12 +14,27 @@ export default function Url(){
         alert('Copied to clipboard');
     }
 
+    const remove = async (id) =>{
+        try{
+            const req = await fetch(`/${id}`, {
+                method: 'DELETE',
+                headers: new Headers()
+            });
+
+            const { msg } = await req.json();
+            alert(msg);
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+
     return (
         <Consumer>
             {({ state }) =>(
                 <React.Fragment>
                     {state.urls.map(({ id, shortUrl, longUrl, date }) =>(
-                        <UrlUI key={id} shortUrl={shortUrl} longUrl={longUrl} copy={copy} date={date} />
+                        <UrlUI key={id} shortUrl={shortUrl} longUrl={longUrl} id={id} remove={remove} copy={copy} date={date} />
                     ))}
                 </React.Fragment>
             )}
